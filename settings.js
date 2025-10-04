2eb241adc5dfe482ef53956f098855cea3e9a62b
daudialubea9-dev <daudialubea9@gmail.com>
2025-10-05 01:41:06 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'DEE XMD',
   author: '‎',
   botName: "DEE ASSASSINt",
-  botOwner: '0711433904', // Your name
+  botOwner: '255711433904', // Your name
   ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
