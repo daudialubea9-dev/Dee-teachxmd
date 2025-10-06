ab5cce0dfe15188484648d2c7d05c07982add478
daudialubea9-dev <daudialubea9@gmail.com>
2025-10-06 13:46:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const settings = {
   author: '‎',
   botName: "DEE MD",
   botOwner: 'DEE MD', // Your name
-  ownerNumber: '255762229202', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '255711433904', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.2.0",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/MR/DEE-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
